3dbc76c503da4a2f4ffbfae909eefd310eb7c9ef
Anishapattanayak <anishapattanayak74@gmail.com>
2026-01-13 12:03:52 +0530

    notes app project

--- react_usestateHook/src/App.jsx
+++ react_usestateHook/src/App.jsx
@@ -3,7 +3,7 @@ import PracticeHook from './components/PracticeHook'
 import UseStateHook from './components/UseStateHook'
 import Array from './components/Array'
 import Batchupdate from './components/Batchupdate'
-import Object  from './components/Object'
+import Object  from './components/Objectgit'
 
 const App = () => {
 
